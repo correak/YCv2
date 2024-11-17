6a6092a532ce6aa38f8ac7bb097b402a47460e2e
PinG0D1 <piero.valverde.cora@gmail.com>
2024-11-17 05:30:06 -0500

    Añadi el "Ver mis inmuebles" con todo el componente, el model, service y demas

---
+++ src/app/model/hu18-dto.ts
@@ -0,0 +1,8 @@
+export class HU18Dto {
+  idInmueble: number;
+  direccionInmueble:string;
+  precioInmueble:number;
+  descripcionPropiedad:string;
+  tipoInmueble:string;
+  estadoPropiedad:string;
+}
